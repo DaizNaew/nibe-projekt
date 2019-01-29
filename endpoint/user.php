d284842ba76fa1d8f1c751cc0c59507246f2871a
DaizNaew <daiznaew@gmail.com>
2019-01-29 12:12:21 +0100

    Rettede user endpoint til at skelne mellem GET og POST

--- endpoint/user.php
+++ endpoint/user.php
@@ -8,53 +8,54 @@ header("Content-Type: application/json; charset=UTF-8");
 include_once '../database.inc';
 include_once '../models/User.php';
 
-$uname = $_GET['brugernavn'];
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $uname = $_GET['brugernavn'];
 
-$database = new Database();
-$db = $database->getConnection();
+    $database = new Database();
+    $db = $database->getConnection();
 
-$user = new User($db);
-// query products
-$stmt = $user->read_uname($uname);
-$num = $stmt->rowCount();
- 
-// check if more than 0 record found
-if($num>0){
+    $user = new User($db);
+    // query products
+    $stmt = $user->read_uname($uname);
+    $num = $stmt->rowCount();
+    
+    // check if more than 0 record found
+    if($num>0){
 
-    $products_arr=array();
-    $products_arr["records"]=array();
- 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-		
-        $product_item=array(
-            "ID" => $ID,
-            "name" => $name,
-            "phoneNumber" => $phoneNumber,
-            "address" => $address,
-            "cardID" => $cardID,
-            "brugernavn" => $brugernavn,
-            "usergruppe" => $usergruppe,
-            "usergruppetitle" => $title,
+        $products_arr=array();
+        $products_arr["records"]=array();
+    
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            
+            $product_item=array(
+                "ID" => $ID,
+                "name" => $name,
+                "phoneNumber" => $phoneNumber,
+                "address" => $address,
+                "cardID" => $cardID,
+                "brugernavn" => $brugernavn,
+                "usergruppe" => $usergruppe,
+                "usergruppetitle" => $title,
+            );
+    
+            array_push($products_arr["records"], $product_item);
+        }
+    
+        // set response code - 200 OK
+        http_response_code(200);
+    
+        // show products data in json format
+        echo json_encode($products_arr);
+    }else{
+    
+        // set response code - 404 Not found
+        http_response_code(404);
+    
+        // tell the user no products found
+        echo json_encode(
+            array("message" => "Ingen brugere fundet")
         );
- 
-        array_push($products_arr["records"], $product_item);
     }
- 
-    // set response code - 200 OK
-    http_response_code(200);
- 
-    // show products data in json format
-    echo json_encode($products_arr);
-}else{
- 
-    // set response code - 404 Not found
-    http_response_code(404);
- 
-    // tell the user no products found
-    echo json_encode(
-        array("message" => "Ingen brugere fundet")
-    );
 }
-
 ?>
