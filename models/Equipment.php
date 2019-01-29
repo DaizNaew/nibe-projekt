138f719936029d25b296e02b44cd0f9a3eb92c2b
DaizNaew <daiznaew@gmail.com>
2019-01-29 12:12:43 +0100

    Tilføjede en insert metode til equipment model

--- models/Equipment.php
+++ models/Equipment.php
@@ -27,7 +27,7 @@ class Equipment {
 	function read($id) {
         $query = "SELECT * FROM " . $this->table_name." INNER JOIN kategori k ON k.ID = equipment.ID";
         
-        if($id !== -1) {
+        if($id != -1) {
             // select query
             $query .= " WHERE equipment.ID = ".$id;
         }
@@ -41,56 +41,22 @@ class Equipment {
         return $stmt;
     }
     
-    function write($name, $number, $address, $cardId, $brugernavn, $adgangskode) {
-        $brugernavn = strtolower($brugernavn);
-        $query = "INSERT INTO " . $this->table_name . " ('name','phoneNumber','address','cardID','brugernavn','adgangskode') VALUES " . "($name, $number, $address, $cardId, $brugernavn, $adgangskode)";
+    function write($post) {
+        $aktivNavn = $post['activeName'];
+        $brand = $post['brandName'];
+        $model = $post['modelName'];
+        $serieNummer = $post['serialNumber'];
+        $assetTag = $post['assetTag'];
+        $stand = $post['condition'];
+        $katID = $post['categoryID'];
+        $inhouse = $post['inHouse'];
+        $query = "INSERT INTO " . $this->table_name . " (aktivNavn,brand,model,serieNummer,assetTag,stand,katID,inhouse) VALUES " . 
+        "('$aktivNavn', '$brand', '$model', '$serieNummer', '$assetTag', '$stand', '$katID', '$inhouse');";
         
         // prepare query statement
         $stmt = $this->conn->prepare($query);
     
         // execute query
-        $stmt->execute();
-    }
-
-    function checkPass($pass,$username) {
-        $query = "SELECT * FROM " . $this->table_name . " WHERE brugernavn = '$username'";
-
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-    
-        // execute query
-        $stmt->execute();
-
-        if(!$stmt->fetch()) {
-            
-            $products_arr["user"]=array();
-            $product_item=array(
-                "ID" => -1,
-                "usergruppe" => -1
-            );
-            array_push($products_arr["user"], $product_item);
-            echo json_encode($products_arr);
-            return;
-        } else {
-            $products_arr["user"]=array();
-            $stmt->closeCursor();
-            $query = "SELECT * FROM " . $this->table_name . " WHERE adgangskode = '$pass'";
-             // prepare query statement
-            $stmt = $this->conn->prepare($query);
-        
-            // execute query
-            $temp = $stmt->execute();
-            if(!$stmt->fetch()) { return false; }
-            $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                extract($row);
-                $product_item=array(
-                    "ID" => $ID,
-                    "usergruppe" => $usergruppe
-                );
-                array_push($products_arr["user"], $product_item);
-            
-            return $products_arr;
-        }
+        return $stmt->execute();
     }
 }
