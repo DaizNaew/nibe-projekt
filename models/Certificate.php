f67f9489a40fd268c13966207abeb017ca4eba43
DaizNaew <daiznaew@gmail.com>
2019-02-04 10:26:44 +0100

    Tilføjede certificat/kørekort model + Endpoint + Funktionerne: Update og Delete

---
+++ models/Certificate.php
@@ -0,0 +1,66 @@
+<?php
+
+class Certificate {
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = ["certifikat","kørekort"];
+ 
+    // object properties
+    public $ID;
+    public $type;
+ 
+    // constructor with $db as database connection
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+	// read products
+	function read($id,$tblID) {
+        $query = "SELECT * FROM " . $this->table_name[$tblID];
+        
+        if($id != '-1') {
+            // select query
+            $query .= " WHERE kategori.ID = ".$id;
+        }
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // execute query
+        $stmt->execute();
+    
+        return $stmt;
+    }
+    
+    function write($post) {
+        $type = $post['type'];
+        $tblID = $post['tableID'];
+        $query = "INSERT INTO " . $this->table_name[$tblID] . " (type) VALUES " . 
+        "('$type');";
+        
+        // prepare query statement
+        $stmt = $this->conn->prepare($query);
+    
+        // execute query
+        return $stmt->execute();
+    }
+
+    function update($post) {
+        $type = $post['type'];
+        $id = $post['ID'];
+        $tblID = $post['tableID'];
+        $query = "UPDATE " . $this->table_name[$tblID] . " SET type = '". $type ."' WHERE ID = '" .$id."'";
+
+        $stmt = $this->conn->prepare($query);
+
+        return $stmt->execute();
+    }
+
+    function delete($id,$tblID) {
+        $query = "DELETE FROM " . $this->table_name[$tblID] . " WHERE ID = ".$id;
+
+        $stmt = $this->conn->prepare($query);
+
+        return $stmt->execute();
+    }
+}
