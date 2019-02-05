74c7ac5427774d5cbdcd6aef931411dfd248cc2c
DaizNaew <daiznaew@gmail.com>
2019-02-05 14:24:51 +0100

    Fjernede funktioner relateret til brugernavn

--- models/User.php
+++ models/User.php
@@ -39,28 +39,6 @@ class User {
     
         return $stmt;
     }
-
-	// read products
-	function read_uname($uname) {
-
-        $uname = strtolower($uname);
-
-        if($uname == -1) {
-            // select all query
-            $query = "SELECT *, u.ID as ID, ug.ID as usergruppeID FROM " . $this->table_name . " u INNER JOIN usergruppe ug ON ug.ID = u.usergruppe";
-        } else {
-            // select query
-            $query = "SELECT *, u.ID as ID, ug.ID as usergruppeID FROM " . $this->table_name . " u INNER JOIN usergruppe ug ON ug.ID = u.usergruppe WHERE brugernavn = "."'$uname'";
-        }
-    
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-    
-        // execute query
-        $stmt->execute();
-    
-        return $stmt;
-    }
     
     function write($name, $number, $address, $cardId) {
         $brugernavn = strtolower($brugernavn);
@@ -71,60 +49,6 @@ class User {
     
         // execute query
         $stmt->execute();
-    }
-
-    function checkPass($pass,$username) {
-        $username = strtolower($username);
-        $query = "SELECT * FROM " . $this->table_name . " WHERE brugernavn = '$username'";
-
-        // prepare query statement
-        $stmt = $this->conn->prepare($query);
-    
-        // execute query
-        $stmt->execute();
-
-        if(!$stmt->fetch()) {
-            
-            $products_arr["user"]=array();
-            $product_item=array(
-                "ID" => -1,
-                "usergruppe" => -1
-            );
-            array_push($products_arr["user"], $product_item);
-            print_r (json_encode($products_arr));
-            return false;
-        } else {
-            $products_arr["user"]=array();
-            $stmt->closeCursor();
-            $query = "SELECT u.ID, name, usergruppe, title FROM " . $this->table_name . " u INNER JOIN usergruppe ug ON ug.ID = usergruppe WHERE adgangskode = '$pass'";
-             // prepare query statement
-            $stmt = $this->conn->prepare($query);
-        
-            // execute query
-            $temp = $stmt->execute();
-            if(!$stmt->fetch()) {
-                $products_arr["user"]=array();
-                $product_item=array(
-                    "ID" => -2,
-                    "usergruppe" => -2
-                );
-                array_push($products_arr["user"], $product_item);
-                print_r (json_encode($products_arr));
-                return false;
-            }
-            $stmt->execute();
-            $row = $stmt->fetch(PDO::FETCH_ASSOC);
-                extract($row);
-                $product_item=array(
-                    "ID" => $ID,
-                    "name" => $name,
-                    "usergruppe" => $usergruppe,
-                    "usergruppetitle" => $title, 
-                );
-                array_push($products_arr["user"], $product_item);
-            
-            return $products_arr;
-        }
     }
 
     function delete($id) {
