e83b80edb12811a0e6a3ef0a168dfb7169114910
DaizNaew <daiznaew@gmail.com>
2019-04-23 12:45:02 +0200

    lavede kommentarer til category funktionerne

--- functions/cats/delete.php
+++ functions/cats/delete.php
@@ -1,6 +1,4 @@
 <?php
-
-
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -11,15 +9,20 @@ include_once '../../models/Category.php';
 
 $id = $_GET['id'];
 
+// Laver en connection til databasen og assigner forbindelsen til en lokal variabel
 $database = new Database();
 $db = $database->getConnection();
 
+// Assigner $Category til at være Category model med selve database forbindelsen som parametre
 $category = new Category($db);
 
-// query products
+// Kører delete funktionen som er inde i Category modellen
 $stmt = $category->delete($id);
+// Hvis databasen siger at der blev slettet noget, så er det $stmt true
 if($stmt) {
+    // set response code - 200 OK
     http_response_code(200);
+    // Fortæl brugeren at der blev slettet det valgte datasæt
     echo json_encode(
         array("message" => "Slettede Kategori fra databasen", 
         "result" => 1
@@ -27,15 +30,13 @@ if($stmt) {
     );
 
 } else {
-    // set response code - 404 Not found
+    // set response code - 200 OK fordi Toft ikke kan håndtere et 404
     http_response_code(200);
 
-    // tell the user no products found
+    // Fortæl brugeren at der ikke kunne slette det valgte datasæt i databasen
     echo json_encode(
         array("message" => "Kunne ikke slette Kategori fra databasen",
         "result" => 0,
         "error" => 'Kategorien bliver brugt af værktøjer i databasen')
     );
 }
-
-?>
