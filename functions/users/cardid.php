d6fd1c242898637da0af434c05f2c0b05bf28d30
DaizNaew <daiznaew@gmail.com>
2019-04-23 10:36:35 +0200

    Lavede kommentarer til alle user funktioner

--- functions/users/cardid.php
+++ functions/users/cardid.php
@@ -9,25 +9,31 @@
 
     $id = $_GET['id'];
 
+    // Laver en connection til databasen og assigner forbindelsen til en lokal variabel
     $database = new Database();
     $db = $database->getConnection();
 
+    // Assigner $User til at være User model med selve database forbindelsen som parametre
     $User = new User($db);
 
+    // Kører getByCard funktionen som er inde i User modellen
     $stmt = $User->getByCard($id);
 
+    // Find antallet af rows med data som blev returneret
     $num = $stmt->rowCount();
     
     // check if more than 0 record found
     if($num>0){
 
-        $products_arr=array();
-        $products_arr["records"]=array();
+        // Laver et nyt array af arrays for at store de returnerede datasæt
+        $users_arr=array();
+        $users_arr["records"]=array();
     
+        // Så længe der findes data i datasættet, så gør vi denne funktion
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            // Derefter udpakkes $row så alle de variabler det indeholder nu er at findes som lokale variabler, så bliver arrayet bare populated med data
             extract($row);
-            
-            $product_item=array(
+            $users_item=array(
                 "ID" => $ID,
                 "name" => $name,
                 "phoneNumber" => $phoneNumber,
@@ -37,20 +43,21 @@
                 "usergruppetitle" => $title,
             );
     
-            array_push($products_arr["records"], $product_item);
+            // Push til arrayet så det kan sendes til brugeren
+            array_push($users_arr["records"], $users_item);
         }
     
         // set response code - 200 OK
         http_response_code(200);
     
-        // show products data in json format
-        echo json_encode($products_arr);
+        // show users data in json format
+        echo json_encode($users_arr);
     }else{
     
         // set response code - 404 Not found
         http_response_code(404);
     
-        // tell the user no products found
+        // Fortæl brugeren at der ikke blev fundet nogle brugere med det medsendte kort ID
         echo json_encode(
             array("message" => "Ingen bruger fundet", "result" => 0)
         );
--- functions/users/update.php
+++ functions/users/update.php
@@ -7,25 +7,30 @@
     include_once '../../database.inc';
     include_once '../../models/User.php';
 
+    // Laver en connection til databasen og assigner forbindelsen til en lokal variabel
     $database = new Database();
     $db = $database->getConnection();
 
+    // Assigner $User til at være User model med selve database forbindelsen som parametre
     $User = new User($db);
 
+    // Kører update funktionen som er inde i User modellen
     $stmt = $User->update($_POST);
 
+    // Hvis databasen siger at der blev opdateret, så er det $stmt true
     if($stmt === true) {
 
+        // Fortæl brugeren at det valgte datasæt blev opdateret i databasen
         http_response_code(200);
         echo json_encode(
             array("message" => "Opdatering blev fuldført", "result" => 1)
         );
 
     } else {
-        // set response code - 404 Not found
+        // set response code - 200 OK fordi Toft ikke kan håndtere et 404
         http_response_code(200);
     
-        // tell the user no products found
+        // Fortæl brugeren at der ikke kunne opdateres det valgte datasæt i databasen
         echo json_encode(
             array("message" => "Kunne ikke Opdatere databasen",
             "result" => 0,
