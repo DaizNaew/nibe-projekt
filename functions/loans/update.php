94d166d66989a0d1bf16e6a5125e4e6f9801ca83
DaizNaew <daiznaew@gmail.com>
2019-04-23 12:25:35 +0200

    Rettede en lille stavefejl

--- functions/loans/update.php
+++ functions/loans/update.php
@@ -29,7 +29,7 @@
         // set response code - 200 OK fordi Toft ikke kan håndtere et 404
         http_response_code(200);
     
-        // Fortæl brugeren at der ikke kunne afleveret det valgte datasæt i databasen
+        // Fortæl brugeren at der ikke kunne opdatere det valgte datasæt i databasen
         echo json_encode(
             array("message" => "Kunne ikke opdaterede Loan i databasen",
             "result" => 0,
