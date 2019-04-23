0abcb51876bb6f27db11020a50eb6883ae7b14bd
DaizNaew <daiznaew@gmail.com>
2019-04-23 12:25:47 +0200

    lavede kommentarer til certs funktioner

--- functions/certs/delete.php
+++ functions/certs/delete.php
@@ -1,6 +1,4 @@
 <?php
-
-
 // required headers
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -13,15 +11,20 @@ $id = $_GET['id'];
 $tblID = $_GET['tableid'];
 $table_name = ["certifikat","kørekort"];
 
+// Laver en connection til databasen og assigner forbindelsen til en lokal variabel
 $database = new Database();
 $db = $database->getConnection();
 
+// Assigner $Certificate til at være Certificate model med selve database forbindelsen som parametre
 $Certificate = new Certificate($db);
 
-// query products
+// Kører delete funktionen som er inde i Certificate modellen
 $stmt = $Certificate->delete($id,$tblID);
+// Hvis databasen siger at der blev slettet noget, så er det $stmt true
 if($stmt) {
+    // set response code - 200 OK
     http_response_code(200);
+    // Fortæl brugeren at der blev slettet det valgte datasæt
     echo json_encode(
         array("message" => "Slettede element fra databasen", 
         "result" => 1
@@ -29,15 +32,13 @@ if($stmt) {
     );
 
 } else {
-    // set response code - 404 Not found
+    // set response code - 200 OK fordi Toft ikke kan håndtere et 404
     http_response_code(200);
 
-    // tell the user no products found
+    // Fortæl brugeren at der ikke kunne slette det valgte datasæt i databasen
     echo json_encode(
         array("message" => "Kunne ikke slette element fra databasen",
         "result" => 0,
         "error" => 'Elementet bliver brugt af en bruger i databasen')
     );
 }
-
-?>
--- functions/certs/update.php
+++ functions/certs/update.php
@@ -7,26 +7,31 @@
     include_once '../../database.inc';
     include_once '../../models/Certificate.php';
 
+    // Laver en connection til databasen og assigner forbindelsen til en lokal variabel
     $database = new Database();
     $db = $database->getConnection();
+    // Table som der kan tjekkes igennem
     $table_name = ["certifikat","kørekort"];
 
+    // Assigner $Certificate til at være Certificate model med selve database forbindelsen som parametre
     $Certificate = new Certificate($db);
 
+    // Kører update funktionen som er inde i Certificate modellen
     $stmt = $Certificate->update($_POST);
-
+    // Hvis databasen siger at der blev opdateret noget, så er det $stmt true
     if($stmt) {
-
+        // set response code - 200 OK
         http_response_code(200);
+        // Fortæl brugeren at der blev opdateret det valgte datasæt
         echo json_encode(
             array("message" => "Opdaterede element i databasen", "result" => 1)
         );
 
     } else {
-        // set response code - 404 Not found
+        // set response code - 200 OK fordi Toft ikke kan håndtere et 404
         http_response_code(200);
     
-        // tell the user no products found
+        // Fortæl brugeren at der ikke kunne opdatere det valgte datasæt i databasen
         echo json_encode(
             array("message" => "Kunne ikke element Kategori i databasen",
             "result" => 0,
