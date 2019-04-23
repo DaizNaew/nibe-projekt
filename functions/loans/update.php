529d0882a32053397c9fbdd3ea83a9ce94e2dde1
DaizNaew <daiznaew@gmail.com>
2019-04-23 12:16:52 +0200

    Lavede kommentarer til loan funktioner

--- functions/loans/update.php
+++ functions/loans/update.php
@@ -7,25 +7,29 @@
     include_once '../../database.inc';
     include_once '../../models/Loaned.php';
 
+    // Laver en connection til databasen og assigner forbindelsen til en lokal variabel
     $database = new Database();
     $db = $database->getConnection();
 
+    // Assigner $Loaned til at være Loaned model med selve database forbindelsen som parametre
     $Loaned = new Loaned($db);
 
+    // Kører update funktionen som er inde i Loaned modellen
     $stmt = $Loaned->update($_POST);
-
+    // Hvis databasen siger at der blev opdateret noget, så er det $stmt true
     if($stmt) {
-
+        // set response code - 200 OK
         http_response_code(200);
+        // Fortæl brugeren at der blev opdateret det valgte datasæt
         echo json_encode(
             array("message" => "Opdaterede Loan i databasen", "result" => 1)
         );
 
     } else {
-        // set response code - 404 Not found
+        // set response code - 200 OK fordi Toft ikke kan håndtere et 404
         http_response_code(200);
     
-        // tell the user no products found
+        // Fortæl brugeren at der ikke kunne afleveret det valgte datasæt i databasen
         echo json_encode(
             array("message" => "Kunne ikke opdaterede Loan i databasen",
             "result" => 0,
