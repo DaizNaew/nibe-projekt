36691a75f6fa45ce15850fafbabf8077204736f6
DaizNaew <daiznaew@gmail.com>
2019-04-23 10:25:12 +0200

    Lavede kommentarer til SP delen

--- functions/callSP.php
+++ functions/callSP.php
@@ -8,19 +8,26 @@ header("Content-Type: application/json; charset=UTF-8");
 // database connection will be here
 include_once '../database.inc';
 
+// Sætter lokale variabler til det der hentes fra GET requestet
+// $sp er navnet på det stored procedure der skal kaldes
 $sp = $_GET['sp'];
 $param = $_GET['param'];
+// Hvis der er flere parametre i GET requestet, delt ad via et ',' 'exploder' vi $param, dvs at hver gang den møder et ','
+// deler den strengen og laver et array af selve strengen
 $params = explode( ',', $param );
 
+// Checker om brugeren har husket at give nogle parametre til stored proceduren
 if(!isset($param)) {
     return 'You need to supply some Param';
 } else {
 
+    // Laver en forbindelse til databasen
     $database = new Database();
     $db = $database->getConnection();
     
-    // constructor with $db as database connection
+
     $sql = "";
+    // Her laves nogle checks og håndtering af parametrene for at sikre os at de bliver formateret korrekt
     if(sizeof($params) < 2) {
         if(!is_int($param)) {
             $sql = 'CALL `'.$sp.'`("'.$param.'")';
@@ -28,11 +35,8 @@ if(!isset($param)) {
             $sql = 'CALL `'.$sp.'`('.$param.')';
         }
     } else {
-
         $sql = "CALL `$sp`($param)";
     }
-    
-    
     
     // prepare query statement
     $stmt = $db->prepare($sql);
@@ -40,17 +44,21 @@ if(!isset($param)) {
     // execute query
     $stmt->execute();
     
+    // Find antallet af rows med data som blev returneret
     $num = $stmt->rowCount();
     
     if($num>0){
     
+        // Laver et nyt array af arrays for at store de returnerede datasæt
         $products_arr=array();
         $products_arr["records"]=array();
      try {
+        // Så længe der findes data i datasættet, så gør vi denne funktion
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
             
             $product_item=$row;
      
+            // Tager dataet og pusher til det array som blev lavet til at store selve datasættet
             array_push($products_arr["records"], $product_item);
             $products_arr['result'] = 1;
         }
@@ -58,9 +66,10 @@ if(!isset($param)) {
         // set response code - 200 OK
         http_response_code(200);
      
-        // show sp data in json format
+        // show sp returned dataset in json format
         echo json_encode($products_arr);
     } catch (Exception $e) {
+        // Ellers vis at der ikke blev fundet noget data
         echo json_encode(
             array("message" => "Ingen Data blev returneret",
                 "result" => "0")
@@ -68,7 +77,7 @@ if(!isset($param)) {
     }
     }else{
      
-        // set response code - 404 Not found
+        // set response code - 200 OK, because Toft wants it to be OK even when error
         http_response_code(200);
      
         // tell the user no sp found
@@ -78,6 +87,3 @@ if(!isset($param)) {
         );
     }
 }
-
-
-?>
