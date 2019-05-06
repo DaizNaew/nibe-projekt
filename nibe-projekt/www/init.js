d40d45509a7fed3a3e937e50137d8f2527e46cd5
DaizNaew <daiznaew@gmail.com>
2019-05-06 09:36:23 +0200

    Revert alt tilbage for at se om det er det der breaker

--- nibe-projekt/www/init.js
+++ nibe-projekt/www/init.js
@@ -9,14 +9,7 @@ requirejs.config({
         app: '../js',
         framework7: '../framework7',
         mysql: '../node_modules/mysql'
-    },
-    // Extended Node modules being loaded
-    packages: [{
-        // Moment is a module in which to manipulate datetimes
-        name: 'moment',
-        location: '../node_modules/moment',
-        main: 'moment'
-    }]
+    }
 });
 
 // Start loading the main app file. Put all of
